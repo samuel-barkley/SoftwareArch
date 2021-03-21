a2cb43a3d8745b84bcf990f26facdc24c8ca4270
Yunus1903 <yunus1903dj@gmail.com>
2021-03-21 19:44:12 +0100

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
-import { ChecksumService } from './checksum.service';
-import { FileDownloadService } from './file-download.service';
-import { FileUploaderServiceService } from './file-uploader-service.service';
+import {Component} from '@angular/core';
+import {FileUploaderServiceService} from './file-uploader-service.service';
 
 @Component({
   selector: 'app-root',
@@ -20,52 +18,43 @@ export class AppComponent {
   getFileLink: string;
   checkSum: string;
 
-  constructor(private fileUploadService: FileUploaderServiceService, private checksumService: ChecksumService)
-  {
+  constructor(private fileUploadService: FileUploaderServiceService) {
     this.errorMsg = false;
   }
 
-  onFileUpload() : void
-  {
-    if (!this.fileObj)
-    {
+  onFileUpload(): void {
+    if (!this.fileObj) {
       this.errorMsg = true;
       return;
     }
     const fileForm = new FormData();
     fileForm.append('file', this.fileObj);
     this.fileUploadService.getSignedUrl(this.fileType).subscribe(res => {
-      const SPLITRES = res.toString().split("||");
+      const SPLITRES = res.toString().split('||');
       this.fileUrl = SPLITRES[0];
       this.getUUID = SPLITRES[1];
       console.log(SPLITRES);
       console.log(this.fileUrl);
       console.log(this.getUUID);
-      
+
       this.fileUploadService.putFileWithUrl(this.fileObj, this.fileUrl).subscribe(
-        (data) =>
-        {
-          console.log(data + " " + res);
+        (data) => {
+          console.log(data + ' ' + res);
         });
     });
   }
 
-  onFilePicked(event) : void
-  {
+  onFilePicked(event): void {
     this.errorMsg = false;
-    const FILE = (event.target as HTMLInputElement).files[0];
-    this.fileObj = FILE;
+    this.fileObj = (event.target as HTMLInputElement).files[0];
     this.fileType = this.fileObj.type;
   }
 
-  onFileGet() : void
-  {
-      this.getFileLink = "https://testbucketthingy.s3.amazonaws.com/" + this.setUUID;
+  onFileGet(): void {
+    this.getFileLink = 'https://testbucketthingy.s3.amazonaws.com/' + this.setUUID;
   }
 
-  onUuidPicked(event) : void
-  {
-    const UUID = (event.target as HTMLInputElement).value;
-    this.setUUID = UUID;
+  onUuidPicked(event): void {
+    this.setUUID = (event.target as HTMLInputElement).value;
   }
 }
